20555ef7b4dd7d8b1f08ea13bcbc615f0def46ff
Hector <norzagaray@me.com>
2018-03-19 13:04:13 -0700

    added landing and app

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Landing from './landing';
+import cookie from "react-cookie";
 import Transactions from './transactions';
 import {BrowserRouter, Route, Router} from 'react-router-dom';
+
+let token = cookie.load('token');
 
 export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <Route exact path='./registration/:auth' component={Landing}/>
-          <Route exact path='./transactions' component={Transactions} />
+          <Route exact path='/registration/:auth' component={Landing}/>
+          <Route exact path='/transactions' component={()=>
+            token
+              ? <Transactions token={token}/>
+              : <Redirect to ='/registration/signup'/> 
+          } />
         </React.Fragment>
       </BrowserRouter>
     );
