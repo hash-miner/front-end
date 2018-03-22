3642054c87a8e6ad35054c78cc111bf7d3fd5b6c
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-03-22 13:32:51 -0700

    styling

---
+++ src/components/home/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavBar from '../navbar';
+
+
+
+export default class Home extends React.Component {
+  render() {
+    return(
+      <div>
+        
+        <div className='h1'>
+          <h1 className='h1heading'>Hash Miner</h1>
+        </div>
+        <div className='h2'>
+          <h1>yyyyyy</h1>
+        </div>
+        
+      </div>
+
+      
+    );
+  }
+}
