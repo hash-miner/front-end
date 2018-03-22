2a6951afb4a77d3f1f3c649c2e9debcae77c8150
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-22 10:04:16 -0700

    ALTERED TRANSACTION ACTION * Changed the transaction action to provide prior information

--- src/action/block-actions.js
+++ src/action/block-actions.js
@@ -6,10 +6,15 @@ export const setTransaction = transaction => ({
 });
 
 export const transactionSendRequest = (transaction, token) => dispatch => {
-  return superagent.post(`${__MAIN_URL__}/transactions`)
-    .set('Authorization', `Bearer ${token}`)
-    .send(transaction)
-    .then(() => {
-      dispatch(setTransaction(transaction));
-    });
-};
+  let searchString = transaction.user_type === 'Distributor'? `batchId=${transaction.batchId}` : transaction.user_type === 'Retailer'? `itemId=${transaction.itemId}` : undefined;
+  return superagent.get(`${__NODE_URL__}/inventory/${searchString}`)
+    .then(result => {
+      if (result.user_type) delete result.user_type;
+      let data = transaction.user_type === 'Grower'? {...transaction} : {...result[0], ...transaction};
+      return superagent.post(`${__MAIN_URL__}/transactions`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(data)
+        .then(() => {
+          dispatch(setTransaction(transaction));
+        });
+    });};
