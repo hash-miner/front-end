bfef226d5e2e171f43e2c0a15e7cdf72145f3627
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-03-22 12:09:11 -0700

    add material ui

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -5,12 +5,11 @@ export default class NavBar extends React.Component {
   render() {
     return(
       <nav>
-        <h1 className="main-header">Hash Mining Hodl Company, LLC</h1>
         <ul className='nav-list'>
           {/* {this.props.currentPage !== 'pretty-view'? <li><Link to='/view/formatted'>Formatted View</Link></li> : undefined}
           {this.props.currentPage !== 'raw-view'? <li><Link to='/view/unformatted'>Unformatted View</Link></li> : undefined} */}
-          {this.props.currentPage !== 'transactions'? <li><Link to='/transactions' refresh='true'>Transactions</Link></li> : undefined}
-          {this.props.currentPage !== 'inventory' ? <li><Link to='/inventory' refresh='true'>Inventory</Link></li> : undefined}
+          {this.props.currentPage !== 'transactions'? <li><Link to='/transactions' refresh='true'>Create Transactions</Link></li> : undefined}
+          {this.props.currentPage !== 'inventory' ? <li><Link to='/inventory' refresh='true'>Search Inventory</Link></li> : undefined}
           {this.props.currentPage !== 'signup' && !this.props.token ? <li><Link to='/registration/signup' refresh='true'>Signup</Link></li> : undefined}
           {this.props.currentPage !== 'signin' && !this.props.token ? <li><Link to='/registration/signin' refresh='true'>Login</Link></li> : undefined}
           {this.props.token ? <li><Link to='/registration/signin' onClick={() => {
