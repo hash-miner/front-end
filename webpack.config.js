564232a64901a8dad2af9d3b851e3f8ca5817dab
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-22 17:13:20 -0700

    fixing it

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query:
           {
             presets: ['react'],
