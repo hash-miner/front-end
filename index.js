5428de54451aa81254788abc27fb7ee2ffc2afd1
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-22 20:10:24 -0700

    added express static to index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static(`${__dirname}/build`));
+
 app.use('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
 app.listen(process.env.PORT, () => console.log(`listening on ${process.env.PORT}`));
