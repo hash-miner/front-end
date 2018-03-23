7382568fc7c5ba73fa9d3018fd8f0643568b2cdc
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-22 19:50:25 -0700

    nope

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 let express = require('express');
 let app = express();
 
-app.use('{,0}', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
+app.use('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
 app.listen(process.env.PORT, () => console.log(`listening on ${process.env.PORT}`));
